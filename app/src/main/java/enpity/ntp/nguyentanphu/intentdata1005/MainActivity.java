6f3c302454cf788f1fae540d62eede1abbcc6177
tanphu <tanphunguyenit@gmail.com>
2019-07-08 20:22:25 +0700

    a

--- app/src/main/java/enpity/ntp/nguyentanphu/intentdata1005/MainActivity.java
+++ app/src/main/java/enpity/ntp/nguyentanphu/intentdata1005/MainActivity.java
@@ -10,4 +10,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //Cách khởi tạo
+    //git init
+    //git add .
+    //git commit -am "a"
+    //git remote add origin https://github.com/tanphu/IntentData1005.git
+    //git push -u origin master
+
 }
